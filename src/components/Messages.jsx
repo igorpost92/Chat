1f1d1072b7d49d441047af702701493e1919bb41
Igor Postolenko <igorpost92@yandex.ru>
2019-03-01 09:53:09 +0300

    add scroll on new message added

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -7,17 +7,51 @@ const mapStateToProps = (state) => {
   return { messages };
 };
 
-const Messages = (props) => {
-  const { messages } = props;
-  return (
-    <div className="d-flex p-2 flex-column align-items-start h-100 mh-100 overflow-auto border">
-      {messages.map((message) => {
-        return (
-          <Message key={message.id} author={message.author} text={message.text} />
-        );
-      })}
-    </div>
-  );
-};
+@connect(mapStateToProps)
+class Messages extends React.Component {
+  state = { scrolled: false }
 
-export default connect(mapStateToProps)(Messages);
+  constructor(props) {
+    super(props);
+    this.container = React.createRef();
+  }
+
+  componentDidUpdate(prevProps) {
+    const container = this.container.current;
+    if (prevProps.messages !== this.props.messages && !this.state.scrolled) {
+      container.scrollTo({
+        top: container.scrollHeight,
+        behavior: 'smooth',
+      });
+    }
+  }
+
+  handleScroll = () => {
+    const { scrollHeight, scrollTop, clientHeight } = this.container.current;
+    const delta = 10;
+    const scrolled = scrollHeight - scrollTop - clientHeight >= delta;
+
+    this.setState((prev) => {
+      if (prev.scrolled === scrolled) {
+        return null;
+      }
+
+      return { scrolled };
+    });
+  }
+
+  render() {
+    const { messages } = this.props;
+    return (
+      <div onScroll={this.handleScroll} ref={this.container} className="d-flex p-2 flex-column align-items-start h-100 mh-100 overflow-auto border">
+        {messages.map((message) => {
+          return (
+            <Message key={message.id} author={message.author} text={message.text} />
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default Messages;
